dbef139279ce7c5e49c1b4e08f7880ad915df85b
Snmtechno <sinem@henryyork.com>
2023-09-14 08:14:47 -0400

    ..

--- src/test/java/Pages/ElementsPage.java
+++ src/test/java/Pages/ElementsPage.java
@@ -1,12 +1,11 @@
 package Pages;
 
 import Utilities.GWD;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.List;
+
 
 public class ElementsPage extends Parent{
 
